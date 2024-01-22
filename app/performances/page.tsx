15ada82ec88ba84fd6d810ca357c4aeea54312f0
mokrinskiy <artyommokrinskiy@gmail.com>
2024-01-22 09:49:45 +0700

    performances adaptive

--- app/performances/page.tsx
+++ app/performances/page.tsx
@@ -6,10 +6,10 @@ const Page = () => {
         <div className="mt-[50px] px-[6vw] mb-[50px]">
             {performances_data.map((performance) => (
                 <div key={performance.title}>
-                    <h1 className="mt-[70px] mb-[20px] text-[70px]">{performance.title}</h1>
+                    <h1 className="mt-[70px] mb-[20px] text-[70px] max-md:text-[40px]">{performance.title}</h1>
                     <div>
                         {performance.text.map((item) => (
-                            <p key={item} className="mt-[15px]">{item}</p>
+                            <p key={item} className="mt-[15px] max-md:text-[15px]">{item}</p>
                         ))}
                     </div>
                 </div>
